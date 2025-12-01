a10fb9b8cda84b65fde1fd057832b350829d900b
Jfernando106 <jose.fernandodasilva@hotmail.com>
2025-11-30 22:06:14 -0400

    feat: auterações de domingo

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Desenvolvedor Front-End especialista em React.js e Next.js e Angular,
+      focado na criação de aplicações performáticas e escaláveis. Minha
+      expertise principal é garantir a navegação fluida e o design responsivo em
+      sistemas de alto tráfego. Sou proficiente em TypeScript e JavaScript, com
+      domínio em frameworks de estilo modernos como Tailwind CSS e Bootstrap.
+      Front-End Core: React.js, Next.js, React Native, Angular. Estilo/UI:
+      Tailwind CSS, Styled Components. Linguagem: TypeScript, JavaScript, HTML5,
+      CSS, Angular.js Back-End: PHP, Laravel, MySQL, Node.js Testes: Cypress,
+      Jest, React Testing Library Outros: Metodologias Ágeis (SCRUM, Kanban),
+      Versionamento (Git, GitHub), Azure DevOps (básico)
+    </div>
+  );
+}
